0af11090692f8f5fe5c090e05f2f63f3be0c935e
Lorran David <lorrantd1@gmail.com>
2026-03-22 05:02:12 -0300

    update nvim config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,10 @@
 return {
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  --   name = "github-theme",
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     require("github-theme").setup({
-  --       -- ...
-  --     })
-  --
-  --     vim.cmd("colorscheme github_light")
-  --   end,
-  -- },
-
   {
-    "Ferouk/bearded-nvim",
-    name = "bearded",
+    "smit4k/shale.nvim",
+    lazy = false,
     priority = 1000,
-    build = function()
-      -- Generate helptags so :h bearded-theme works
-      local doc = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "bearded", "doc")
-      pcall(vim.cmd, "helptags " .. doc)
-    end,
     config = function()
-      require("bearded").setup({
-        flavor = "arc-blueberry", -- any flavor slug
-      })
-      vim.cmd.colorscheme("bearded")
+      vim.cmd.colorscheme("shale")
     end,
   },
 }
